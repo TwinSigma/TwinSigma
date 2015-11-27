a2f5e2613f0500e3a6dd183ec6f262f2430f4646
Noah <xlash19o13@gmail.com>
2015-11-27 15:30:21 -0500

    Allowed for size setting

--- src/twinsigma/com/gamename/elements/Background.java
+++ src/twinsigma/com/gamename/elements/Background.java
@@ -3,14 +3,24 @@ package twinsigma.com.gamename.elements;
 import twinsigma.com.gamename.graphic.Sprite;
 import twinsigma.com.gamename.graphic.gui.Window;
 
-public class Background extends MovableElement{
+public class Background extends SceneElement{
 
 	public Background(double x, double y, Sprite img) {
 		super(x, y, img);
 	}
 	
+	public Background(double x, double y, double width, double height, double rot, Sprite img) {
+		super(x, y, width, height, rot, img);
+	}
+	
 	public void centerOnElement(MovableElement e){
 		this.setLocation(e.x + e.width/2 - Window.WIDTH/2, e.y + e.height/2 - Window.HEIGHT/2);
+	}
+	
+	@Override
+	public void screenChange(){
+		this.width = Window.WIDTH;
+		this.height = Window.HEIGHT;
 	}
 
 }
