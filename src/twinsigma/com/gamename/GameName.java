8e361dc25c9b805ff52e89c445479158d0449550
Noah <xlash19o13@gmail.com>
2015-12-23 23:50:30 -0500

    Removed unnecessary imports

--- src/twinsigma/com/gamename/GameName.java
+++ src/twinsigma/com/gamename/GameName.java
@@ -1,15 +1,13 @@
 package twinsigma.com.gamename;
 
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import twinsigma.com.gamename.elements.Background;
+import twinsigma.com.gamename.elements.PerspectiveEnum;
 import twinsigma.com.gamename.elements.Player;
 import twinsigma.com.gamename.elements.Scene;
 import twinsigma.com.gamename.graphic.gui.Gui;
 import twinsigma.com.gamename.graphic.gui.LogoGui;
-import twinsigma.com.gamename.graphic.gui.TextFieldGui;
 import twinsigma.com.gamename.graphic.gui.Window;
 import twinsigma.com.gamename.input.Input;
 import twinsigma.com.gamename.managers.ResourceManager;
@@ -46,7 +44,7 @@ public class GameName{
 				};
 				window.addKeyListener(new Input());
 				resourceManager.loadAllResources();
-				scene = new Scene(new Background(0, 0, Window.WIDTH, Window.HEIGHT, 0, ResourceManager.grass),
+				scene = new Scene(PerspectiveEnum.OVERHEAD, new Background(0, 0, Window.WIDTH, Window.HEIGHT, 0, ResourceManager.grass),
 						new Player(10, 10, 50, 50, 0, ResourceManager.player));
 				gameLoop.start();
 			}
